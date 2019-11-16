8739124ffa851f933742a6460966272380e2ee79
domotord90 <jazzmaster@freemail.com>
2019-11-16 16:05:14 +0100

    Updates

---
+++ precache-manifest.4a3aaaed5707d368fa66a8a53a7957af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181a35bd7dd47041b8613bfd48bb1680",
+    "url": "/react-promodoro-clock/index.html"
+  },
+  {
+    "revision": "d3f0d06e7b2a74cd1a7f",
+    "url": "/react-promodoro-clock/static/css/main.936f6ffb.chunk.css"
+  },
+  {
+    "revision": "1a61c4f65511a54b1d49",
+    "url": "/react-promodoro-clock/static/js/2.18a00cf8.chunk.js"
+  },
+  {
+    "revision": "d3f0d06e7b2a74cd1a7f",
+    "url": "/react-promodoro-clock/static/js/main.482f7b07.chunk.js"
+  },
+  {
+    "revision": "49660ffe8512104180b1",
+    "url": "/react-promodoro-clock/static/js/runtime~main.31482492.js"
+  },
+  {
+    "revision": "ee9072cfc9314d18a8e614104a805ab1",
+    "url": "/react-promodoro-clock/static/media/DIGITALDREAM.ee9072cf.ttf"
+  },
+  {
+    "revision": "8ee39c4e8d0a8dc32b841f060f952a5b",
+    "url": "/react-promodoro-clock/static/media/beep.8ee39c4e.mp3"
+  }
+]);
